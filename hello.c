d5e0f31f4e5da496a917a86458f73a53cdefb845
plvo <plvdev@proton.me>
2023-10-26 14:52:44 +0200

    test addition scanf

--- hello.c
+++ hello.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(){
-    printf("\n\nHello");
+    float nb1;
+    float nb2;
+    printf("\nHello\n\n");
+    printf("We will make some additions\n");
+    printf("Give a number:\t");
+    scanf("%f",&nb1);
+    printf("Give a second number: \t");
+    scanf("%f",&nb2);
+    float som = nb1 + nb2;
+    printf("The sum of %f and %f is equal to %f", nb1, nb2, som);
     return 0;
 }
