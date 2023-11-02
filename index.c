9d358704f8d8e847d005f91847c4e9aae324fd60
Neysixx <kylliansenrens3004@gmail.com>
2023-11-02 16:09:23 +0100

    edit

--- index.c
+++ index.c
@@ -3,41 +3,54 @@
 #include <string.h>
 
 #define MAX_LINE_LENGTH 1024
+#define MAX_SENSOR_COUNT 13
+#define MAX_FRIGO 4
 
-int VENTILATION_START(int variable){
+int VENTILATION_START(int variable) {
     return variable;
 }
-int BADGE_ON(int variable){
+
+int BADGE_ON(int variable) {
     return variable;
 }
-int ENTER_DOOR_OPEN(int variable){
+
+int ENTER_DOOR_OPEN(int variable) {
     return variable;
 }
-int ENTER_DOOR_CLOSE(int variable){
+
+int ENTER_DOOR_CLOSE(int variable) {
     return variable;
 }
-int TIMER_ON(int variable){
+
+int TIMER_ON(int variable) {
     return variable;
 }
-int TEMP_START(int variable){
+
+int TEMP_START(int variable) {
     return variable;
 }
-int TIMER_CHECK(int variable){
+
+int TIMER_CHECK(int variable) {
     return variable;
 }
-int EXIT_DOOR_OPEN(int variable){
+
+int EXIT_DOOR_OPEN(int variable) {
     return variable;
 }
-int EXIT_DOOR_CLOSE(int variable){
+
+int EXIT_DOOR_CLOSE(int variable) {
     return variable;
 }
-int BADGE_OFF(int variable){
+
+int BADGE_OFF(int variable) {
     return variable;
 }
-int TEMP_END(int variable){
+
+int TEMP_END(int variable) {
     return variable;
 }
-int VENTILATION_EXIT(int variable){
+
+int VENTILATION_EXIT(int variable) {
     return variable;
 }
 
@@ -57,26 +70,34 @@ int main() {
     }
 
     char ligne[MAX_LINE_LENGTH];
+    struct Capt capteurs[MAX_SENSOR_COUNT];
 
     if (fgets(ligne, sizeof(ligne), fichier) != NULL) {
         char *token = strtok(ligne, ",");
-        struct Capt capteurs[12];
-        
         int i = 0;
-        while (token != NULL) {
-            printf("token actuel : %s\n", token);
+
+        while (token != NULL && i < MAX_SENSOR_COUNT) {
             capteurs[i].variable = 0;
             capteurs[i].nomCapt = strdup(token);
-            printf("token actuel 222 : %s\n\n", capteurs[i].nomCapt);
+            printf("Capteur ajoute : %s\n", capteurs[i].nomCapt);
             token = strtok(NULL, ",");
             i++;
         }
 
-        for (int j = 0; j < 13; j++){
-            printf("%d) %s\n", j, capteurs[j].nomCapt);
+        int k=0;
+        while (fgets(ligne, sizeof(ligne), fichier) != NULL && k < MAX_FRIGO) {
+            token = strtok(ligne, ",");
+            for (int j = 0; j < i; j++) {
+                if (token != NULL) {
+                    int valeur = atoi(token);
+                    capteurs[j].variable = valeur;
+                    printf("%s : %d\n", capteurs[j].nomCapt, capteurs[j].variable);
+                    token = strtok(NULL, ",");
+                }
+            }
+            k++;
         }
     }
 
-    fclose(fichier);
     return 0;
 }
