533baaef7450497d5c2bf226eca3bad2ec1ea39b
pelavo <113256696+plv7@users.noreply.github.com>
2023-11-02 15:15:35 +0100

    Update index.c

--- index.c
+++ index.c
@@ -4,98 +4,6 @@
 
 #define MAX_LINE_LENGTH 1024
 
-int VENTILATION_START(int variable);
-int BADGE_ON(int variable);
-int ENTER_DOOR_OPEN(int variable);
-int ENTER_DOOR_CLOSE(int variable);
-int TIMER_ON(int variable);
-int TEMP_START(int variable);
-int TIMER_CHECK(int variable);
-int EXIT_DOOR_OPEN(int variable);
-int EXIT_DOOR_CLOSE(int variable);
-int BADGE_OFF(int variable);
-int TEMP_END(int variable);
-int VENTILATION_EXIT(int variable);
-
-
-
-struct Capt {
-    int variable;
-    int (*fonction)(int);
-};
-
-int main() {
-    FILE *fichier;
-    fichier = fopen("config.csv", "r");
-
-    if (fichier == NULL) {
-        printf("Impossible d'ouvrir le fichier.\n");
-        return 1;
-    }
-
-    char ligne[MAX_LINE_LENGTH];
-    if (fgets(ligne, sizeof(ligne), fichier) != NULL) {
-        char *token = strtok(ligne, ",");
-        struct Capt capteurs[12];
-
-        int i = 0;
-        while (token != NULL) {
-            printf("En-tête : %s\n", token);
-
-            capteurs[i].variable = 0;
-            if (strcmp(token, "VENTILATION_START")) {
-                capteurs[i].fonction = VENTILATION_START;
-                printf("%s\n",token);
-            }
-            else if (strcmp(token, "BADGE_ON") == 0) {
-                capteurs[i].fonction = BADGE_ON;
-            }
-            else if (strcmp(token, "ENTER_DOOR_OPEN") == 0) {
-                capteurs[i].fonction = ENTER_DOOR_OPEN;
-            }
-            else if (strcmp(token, "ENTER_DOOR_CLOSE") == 0) {
-                capteurs[i].fonction = ENTER_DOOR_CLOSE;
-            }
-            else if (strcmp(token, "TIMER_ON") == 0) {
-                capteurs[i].fonction = TIMER_ON;
-            }
-            else if (strcmp(token, "TEMP_START") == 0) {
-                capteurs[i].fonction = TEMP_START;
-            }
-            else if (strcmp(token, "TIMER_CHECK") == 0) {
-                capteurs[i].fonction = TIMER_CHECK;
-            }
-            else if (strcmp(token, "EXIT_DOOR_OPEN") == 0) {
-                capteurs[i].fonction = EXIT_DOOR_OPEN;
-            }
-            else if (strcmp(token, "EXIT_DOOR_CLOSE") == 0) {
-                capteurs[i].fonction = EXIT_DOOR_CLOSE;
-            }
-            else if (strcmp(token, "BADGE_OFF") == 0) {
-                capteurs[i].fonction = BADGE_OFF;
-            }
-            else if (strcmp(token, "TEMP_END") == 0) {
-                capteurs[i].fonction = TEMP_END;
-            }
-            else if (strcmp(token, "VENTILATION_EXIT") == 0) {
-                capteurs[i].fonction = VENTILATION_EXIT;
-            }
-
-            token = strtok(NULL, ",");
-            i++;
-        }
-        
-        for (i = 0; i < 12; i++) {
-            int resultat = capteurs[i].fonction(capteurs[i].variable);
-            printf("Résultat pour %s : %d\n", token, resultat);
-        }
-    }
-
-    fclose(fichier);
-    return 0;
-}
-
-
 int VENTILATION_START(int variable){
     return variable;
 }
@@ -131,4 +39,56 @@ int TEMP_END(int variable){
 }
 int VENTILATION_EXIT(int variable){
     return variable;
+}
+
+struct Capt {
+    char *nomCapt;
+    int variable;
+    int (*fonction)(int);
+};
+
+int main() {
+    FILE *fichier;
+    fichier = fopen("./output/config.csv", "r");
+
+    if (fichier == NULL) {
+        printf("Impossible d'ouvrir le fichier.\n");
+        return 1;
+    }
+
+    char ligne[MAX_LINE_LENGTH];
+
+    if (fgets(ligne, sizeof(ligne), fichier) != NULL) {
+        char *token = strtok(ligne, ",");
+        struct Capt capteurs[12];
+        
+        int i = 0;
+
+        while (token != NULL) {
+            printf("token actuel : %s\n", token);
+
+            capteurs[i].variable = 0;
+            capteurs[i].nomCapt = token;
+
+            printf("token actuel 222 : %s\n\n", capteurs[i].nomCapt);
+
+            token = strtok(NULL, ",");
+
+            i++;
+            // int resultat = capteurs[i].fonction(capteurs[i].variable);
+            // printf("Resultat pour %s : %d \n\n\n", token, resultat);
+        }
+
+        for (int i = 0; i < 13; i++){
+            printf("%d) %s\n", i, capteurs[i].nomCapt);
+        }
+        
+        // for (i = 0; i < 12; i++) {
+        //     int resultat = capteurs[i].fonction(capteurs[i].variable);
+        //     printf("Resultat pour %s : %d \n", token, resultat);
+        // }
+    }
+
+    fclose(fichier);
+    return 0;
 }
