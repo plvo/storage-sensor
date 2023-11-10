7e79cca174b728b93c394e0e29bfe3de528596a4
Neysixx <kylliansenrens3004@gmail.com>
2023-11-10 10:41:55 +0100

    edit Récupération X Y

--- main.c
+++ main.c
@@ -12,6 +12,42 @@ struct Capt {
     int (*fonction)(int);
 };
 
-int main(){
-    nigga power
+int main() {
+    // Ouvrir le fichier CSV en mode lecture
+    FILE *fichier = fopen("config.csv", "r");
+    
+    if (fichier == NULL) {
+        printf("Impossible d'ouvrir le fichier.\n");
+        return 1;
+    }
+
+    int nombre_lignes = 0;
+    int nombre_colonnes = 0;
+    char ligne[1024];  // Vous pouvez ajuster la taille en fonction de vos besoins
+
+    // Lire le fichier ligne par ligne
+    while (fgets(ligne, sizeof(ligne), fichier) != NULL) {
+        nombre_lignes++;
+
+        // La première ligne est supposée contenir les en-têtes des colonnes
+        if (nombre_lignes == 1) {
+            char *token = strtok(ligne, ",");
+            while (token != NULL) {
+                nombre_colonnes++;
+                token = strtok(NULL, ",");
+            }
+        }
+    }
+
+    fclose(fichier);
+
+    if (nombre_lignes > 0 && nombre_colonnes > 0) {
+        nombre_lignes = nombre_lignes - 1;
+        printf("Nombre de lignes : %d\n", nombre_lignes);
+        printf("Nombre de colonnes : %d\n", nombre_colonnes);
+    } else {
+        printf("Le fichier est vide ou ne contient pas de données CSV valides.\n");
+    }
+
+    return 0;
 }
