450db7f6fd96abb5095aea6e7cb843f147a089c3
pelavo <113256696+plv7@users.noreply.github.com>
2023-11-10 10:38:31 +0100

    Create main.c

---
+++ main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAX_LINE_LENGTH 1024
+#define MAX_SENSOR_COUNT 13
+#define MAX_FRIGO 5
+
+struct Capt {
+    char *nomCapt;
+    int variable;
+    int (*fonction)(int);
+};
+
+int main(){
+    nigga power
+}
