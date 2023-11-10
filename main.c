38624c97ad07a933a372603c4082b12d46332952
Neysixx <kylliansenrens3004@gmail.com>
2023-11-10 12:31:13 +0100

    edit function

--- main.c
+++ main.c
@@ -13,6 +13,145 @@ struct Capt {
     int (*fonction)(int);
 };
 
+void write_CSV(const char *data) {
+    // Open file
+    FILE *file = fopen("log.csv", "a"); 
+    if (file == NULL) {
+        perror("fichier introuvable");
+        return;
+    }
+    // Écrire donnée et fermer file
+    fprintf(file, "%s\n", data);
+    fclose(file);
+}
+
+
+// Déclaration des fonctions renvoyant les valeurs des différents capteurs :
+// Chaque fonction doit renvoyer 0 si la valeur n'est pas celle attendu et 1 si tout est bon
+
+//capteur ventilation
+int VENTILATION_START(int variable) {
+    if (variable==1){
+        write_CSV("La ventillation du frigo fonctionne - OK \n")
+        return 1;
+    } else {
+        write_CSV("La ventillation du frigo ne fonctionne pas - STOP (error) \n")
+        return 0;
+    }
+}
+//capteur du badge
+int BADGE_ON(int variable){
+    if (variable==1){
+        write_CSV("Le badge a ete depose - OK \n")
+        return 1;
+    } else {
+        write_CSV("Le badge n'a pas ete depose - STOP (error) \n")
+        return 0;
+    }
+}
+//capteur de l'ouverure de la porte du frigo
+int ENTER_DOOR_OPEN(int variable){
+    if (variable==1){
+        write_CSV("La porte s'est ouverte - OK \n")
+        return 1;
+    } else {
+        write_CSV("La porte ne s'est pas ouverte - STOP (error) \n")
+        return 0;
+    }
+}
+
+//capteur de la fermeture de la porte du frigo
+int ENTER_DOOR_CLOSE(int variable){
+    if (variable==1){
+        write_CSV("La porte a ete refermee - OK \n")
+        return 1;
+    } else {
+        write_CSV("La porte n'a pas ete refermee - STOP (error) \n")
+        return 0;
+    }
+}
+//lancement du chronomètre du frigo
+int TIMER_ON(int variable){
+    if (variable==1){
+        write_CSV("Le chronomètre a ete lance - OK \n")
+        return 1;
+    } else {
+        write_CSV("Le chronomètre ne s'est pas lance - STOP (error) \n")
+        return 0;
+    }
+}
+//capteur de la température du frigo avant toute action
+int TEMP_START(int variable){
+    if (variable==-40){
+        write_CSV("La temperature est valide - OK \n")
+        return 1;
+    } else {
+        write_CSV("La temperature n'est pas bonne - STOP (error) \n")
+        return 0;
+    }
+}
+
+//vérification du chronomètre
+int TIMER_CHECK(int variable){
+    if (variable<=10){
+        write_CSV("L'employe est reste %d min dans le frigo - OK \n",variable)
+        return 1;
+    } else {
+        write_CSV("L'employe est reste %d min dans le frigo. Une alerte a ete declenchee - STOP (error) \n", variable)
+        return 0;
+    }
+}
+// capteur d'ouverture de porte a la sortie
+int EXIT_DOOR_OPEN(int variable){
+    if (variable==1){
+        write_CSV("La porte a ete ouverte - OK \n")
+        return 1;
+    } else {
+        write_CSV("La porte n'a pas ete ouverte - STOP (error) \n")
+        return 0;
+    }
+}
+//capteur de la fermeture de la porte a la sortie
+int EXIT_DOOR_CLOSE(int variable){
+    if (variable==1){
+        write_CSV("La porte a ete refermee - OK \n")
+        return 1;
+    } else {
+        write_CSV("La porte n'a pas ete refermee - STOP (error) \n")
+        return 0;
+    }
+}
+//verification que le badge est recupere
+int BADGE_OFF(int variable){
+    if (variable==1){
+        write_CSV("Le badge a ete recupere - OK \n")
+        return 1;
+    } else {
+        write_CSV("Le badge n'a pas ete recupere - STOP (error) \n")
+        return 0;
+    }
+}
+//vérification de la température a la fin de toute action
+int TEMP_END(int variable){
+    if (variable==-40){
+        write_CSV("La temperature est correcte apres passage - OK \n")
+        return 1;
+    } else {
+        write_CSV("La temperature n'est pas correcte apres passage - STOP (error) \n")
+        return 0;
+    }
+}
+//vérification de l'état de la ventilation a la sortie
+int VENTILATION_EXIT(int variable){
+    if (variable==1){
+        write_CSV("La ventilation est toujours active apres passage - OK \n")
+        return 1;
+    } else {
+        write_CSV("La ventilation n'est plus active apres passage - STOP (error) \n")
+        return 0;
+    }
+}
+
 int main() {
     FILE *fichier;
     fichier = fopen("./config.csv", "r");
