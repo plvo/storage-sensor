fdb71ffe3a0f6d08d3b91238bbde2ded4c59c6aa
julian <pimsras@gmail.com>
2023-11-30 14:11:43 +0100

    random-02

--- randomizer.c
+++ randomizer.c
@@ -1,3 +1,4 @@
+//bibliotheque 
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
@@ -7,32 +8,29 @@ int randomIntGen(int min, int max) {
         fprintf(stderr, "Erreur : Les bornes ne sont pas valides.\n");
         exit(EXIT_FAILURE);
     }
-    srand((unsigned int)time(NULL));
     int plage = max - min + 1;
     int nombre = rand() % plage + min;
     return nombre;
 }
 
-int* arrayValCapt(int min, int max) {
-    // Allouer dynamiquement un tableau de taille 13
-    int *tableau = malloc(13 * sizeof(int));
+int* arrayValCapt(int min, int max, int taille) {
+    // Allouer dynamiquement un tableau de taille "taille"
+    int *tableau = malloc(taille * sizeof(int));
 
     if (tableau == NULL) {
         fprintf(stderr, "Erreur d'allocation mémoire.\n");
         exit(EXIT_FAILURE);
     }
 
-    for (int i = 0; i < 13; ++i) {
+    for (int i = 0; i < taille; ++i) {
         tableau[i] = rand() % (max - min + 1) + min;
     }
-
     return tableau;
 }
 
 void ajouterLigneCSV(const char *nomFichier, int tableau[], int taille) {
     // Ouvrir le fichier en mode ajout (a) pour ajouter du contenu à la fin
     FILE *fichier = fopen(nomFichier, "a");
-
     if (fichier == NULL) {
         perror("Erreur lors de l'ouverture du fichier");
         return;
@@ -47,32 +45,39 @@ void ajouterLigneCSV(const char *nomFichier, int tableau[], int taille) {
             fprintf(fichier, ",");
         }
     }
-
     fprintf(fichier, "\n");
 
     // Fermer le fichier
     fclose(fichier);
-
     printf("Ligne ajoutée au fichier CSV avec succès.\n");
 }
 
-int main() {
+int randomArray(int arrayMin, int arrayMax) {
     const int taille = 13;
     const char *nomFichierCSV = "testconfig.csv";
-    int *tab1 = arrayValCapt(2, 20);
 
-    // Afficher le contenu du tableau
+    // Initialiser le générateur de nombres aléatoires avec une nouvelle graine
+    srand((unsigned int)time(NULL));
+
+    // Générer le tableau random
+    int *tab1 = arrayValCapt(arrayMin, arrayMax, taille);
+
+    // Afficher le tableau
     printf("Tableau : ");
     for (int i = 0; i < taille; ++i) {
         printf("%d ", tab1[i]);
     }
     printf("\n");
 
-    // Ajouter la ligne CSV avec le contenu du tableau
+    // Ajouter la ligne dans le CSV 
     ajouterLigneCSV(nomFichierCSV, tab1, taille);
 
-    // Libérer la mémoire allouée dynamiquement
     free(tab1);
 
+    return 0;
+}
+
+int main(){
+    randomArray(2, 20);
     return 0;
 }
